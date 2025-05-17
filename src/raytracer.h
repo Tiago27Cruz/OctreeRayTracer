27dbf1261b7f26af1c8cb2801337fbcde10287cd
Tiago Cruz <tiagocruz057@gmail.com>
2025-05-17 18:29:50 +0100

    added new class so main is not so cluttered

---
+++ src/raytracer.h
@@ -0,0 +1,52 @@
+#ifndef RAYTRACER_H
+#define RAYTRACER_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "opengl/Shader.h"
+#include "opengl/Mesh.h"
+#include "opengl/camera.h"
+#include "octree.h"
+#include "sphere.h"
+#include <vector>
+
+class Raytracer {
+    public:
+        Raytracer();
+        ~Raytracer();
+
+        bool initialize();
+        void run();
+    private:
+        // Window and OpenGL
+        GLFWwindow* window;
+        int width, height;
+        
+        // Rendering
+        Mesh* raytracingQuad;
+        Shader* shader;
+        
+        // Scene
+        std::vector<Sphere> spheres;
+        Octree octree;
+        
+        // GPU buffer objects
+        GLuint spheresSSBO;
+        GLuint sphereDataSSBO;
+        GLuint sphereData2SSBO;
+        GLuint octreeNodesSSBO;
+        GLuint octreeNodes2SSBO;
+        GLuint octreeCountsSSBO;
+        GLuint objectIndicesSSBO;
+
+        // methods
+        void setupQuad();
+        void setupShader();
+        void setupScene();
+        void setupBuffers();
+        void cleanupBuffers();
+        vector<Sphere> generateSpheres();
+
+};
+
+#endif // RAYTRACER_H
